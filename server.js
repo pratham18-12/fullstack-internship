bfe4913f246259f2c8c3f908fe583887a2bb157c
Aizen187 <134590403+Aizen187@users.noreply.github.com>
2026-07-31 23:59:09 +0530

    Day 12 Deployment

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 let blogs = [];
 
 // Home
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 // Get Blogs
