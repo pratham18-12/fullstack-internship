23296e2b11a1429db8ed5f870ef1d56ae937cdf1
Aizen187 <134590403+Aizen187@users.noreply.github.com>
2026-07-31 21:41:33 +0530

    Day 5 Express Routes Completed

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Welcome to My Express Server");
+});
+
+app.get("/blog", (req, res) => {
+    res.send("This is the Blog Page");
+});
+
+app.post("/contact", (req, res) => {
+    const { name, email } = req.body;
+
+    res.json({
+        message: "Form Submitted Successfully",
+        name,
+        email
+    });
 });
 
 app.listen(3000, () => {
-    console.log("Server running...");
+    console.log("Server running on port 3000");
 });
